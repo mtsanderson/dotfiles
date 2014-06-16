0fca1f052a9473c22ddd10c8450c216f34dfc857
Michael Sanderson <michael@unt.edu>
2014-06-16 16:10:49 -0500

    cleanup

--- scripts/getweather.sh
+++ scripts/getweather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s http://www.wunderground.com/US/TX/denton.html | grep tempActual | cut -d '"' -f18
+curl -s http://www.wunderground.com/US/TX/denton.html | grep -A1 '<span class="wx-data" data-station="KTXDENTO11" data-variable="feelslike">' | grep wx-value | sed "s/		<span class=\"wx-value\">//" | cut -d '<' -f1
